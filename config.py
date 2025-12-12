506f913db99391ba9e974e941bb4f0a69a2c489b
Pavel <100708895+MrBombastick08@users.noreply.github.com>
2025-12-12 05:26:43 +0300

    Скрыт Database URI

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:p4v17102006@localhost:5432/furniture_company'
+    SQLALCHEMY_DATABASE_URI = 'TOP_SECRET'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
